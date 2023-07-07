9bc7f760da7f068d9b80b3408acabcaef3245a2c
dpluong <duyphuocnk92@gmail.com>
2023-07-06 23:38:15 -0700

    Initial commit

---
+++ PostureVR/Assets/Scripts/HeightTesting.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HeightTesting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public InputDeviceCharacteristics controllerCharacteristics;
+    private InputDevice targetDevice;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
